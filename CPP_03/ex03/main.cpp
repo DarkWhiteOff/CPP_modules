12ed54b123a29a646b068ef78c055e775b74693a
Ziggy Amgar <zamgar@f2r12s16.clusters.42paris.fr>
2025-06-17 15:14:23 +0200

    good

--- CPP_03/ex03/main.cpp
+++ CPP_03/ex03/main.cpp
@@ -5,28 +5,8 @@
 
 int main(void)
 {
-    // ClapTrap Walter("Walter");
-    // ScavTrap Jesse("Jesse");
-    // FragTrap Mike("Mike");
-    DiamondTrap Saul;
-    
-    // Walter.attack("Jesse"); // first attack
-    // Jesse.takeDamage(0);
-    // Jesse.beRepaired(0); // -1 Epts
-    // Jesse.attack("Walter"); // -1 Epts
-    // Walter.takeDamage(20);
-    // Walter.attack("Jesse"); // 2 -> no hit points left
-    // Jesse.attack("Mike");
-    // Mike.takeDamage(20);
-    // Mike.attack("Jesse");
-    // Jesse.takeDamage(30);
-    // Saul.attack("Jesse");
-    // Saul.attack("Jesse");
-    // Saul.attack("Jesse");
-    // Jesse.takeDamage(30);
-    // Jesse.takeDamage(30);
-    // Jesse.takeDamage(30);
-    
+    DiamondTrap Saul("Saul");
+
     Saul.whoAmI();
     return (0);
 }
