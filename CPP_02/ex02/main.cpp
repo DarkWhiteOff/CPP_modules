78417d5d69ae57ec5e7d34a25bee4354860f4f47
Ziggy Amgar <zamgar@f2r12s16.clusters.42paris.fr>
2025-06-17 14:23:34 +0200

    02 ok

--- CPP_02/ex02/main.cpp
+++ CPP_02/ex02/main.cpp
@@ -13,45 +13,51 @@ int main(void)
     std::cout << b << std::endl;
     std::cout << Fixed::max(a, b) << std::endl;
 
+    std::cout << std::endl;
     Fixed c(10.47f);
-    std::cout << "c : " << c << std::endl;
+    std::cout << "c begin : " << c << std::endl;
 
     Fixed d = ++c;
-    std::cout << "d : " << d << std::endl;
+    std::cout << "d with ++c on it : " << d << std::endl;
     c--;
 
     Fixed i = c++;
-    std::cout << "i : " << i << std::endl;
+    std::cout << "i without ++c on it (because c++) : " << i << std::endl;
 
-    ++c;
+    std::cout << std::endl;
+    c++;
     std::cout << "c : " << c << std::endl;
     std::cout << "d : " << d << std::endl;
-    std::cout << "min : " << Fixed::min(c, d) << std::endl;
-    std::cout << "max : " << Fixed::max(c, d) << std::endl;
+    std::cout << "min : " << Fixed::min(c, d) << " (d)" << std::endl;
+    std::cout << "max : " << Fixed::max(c, d) << " (c)" << std::endl;
 
+    std::cout << std::endl;
     Fixed e(16.16f);
     Fixed f = e++;
 
-    std::cout << "e : " << e << std::endl;
-    std::cout << "f : " << f << std::endl;
+    std::cout << "e (larger cause e++) : " << e << std::endl;
+    std::cout << "f (smaller cause wasn't ++e): " << f << std::endl;
 
     Fixed g(17);
     Fixed h(Fixed(2.2f) + Fixed(13.5f));
 
     std::cout << "g : " << g << std::endl;
     std::cout << "h : " << h << std::endl;
+
+    std::cout << std::endl;
     std::cout << "h / 2 : " << h / 2 << std::endl;
     std::cout << "h * 2 : " << h * 2 << std::endl;
     std::cout << "g + 8 : " << g + 8 << std::endl;
     std::cout << "g - 4 : " << g - 4 << std::endl;
 
+    std::cout << std::endl;
     Fixed test(17);
-    std::cout << g.operator!=(h) << std::endl;
-    std::cout << g.operator==(test) << std::endl;
-    std::cout << g.operator>=(h) << std::endl;
-    std::cout << g.operator<=(h) << std::endl;
-    std::cout << g.operator<=(h/2) << std::endl;
-    std::cout << g.operator>=(h/2) << std::endl;
+    std::cout << g.operator!=(h) << " (g != h is true) " << g << " != " << h << std::endl;
+    std::cout << g.operator==(test) << " (g == test is true) " << g << " == " << test << std::endl;
+    std::cout << g.operator>=(h) << " (g >= h is true) " << g << " >= " << h << std::endl;
+    std::cout << g.operator<=(h) << " (g <= h is false) " << g << " <= " << h << std::endl;
+    std::cout << g.operator<=(h/2) << " (g <= h/2 is false) " << g << " <= " << h / 2 << std::endl;
+    std::cout << g.operator>=(h/2) << " (g >= h is true) " << g << " >= " << h / 2 << std::endl;
 
     return (0);
 }
